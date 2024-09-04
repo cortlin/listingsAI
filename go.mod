f27c9e3a5c30400f271c65a1a18fe7f81ba32770
Cortlin Sawyer <cortlin@Cortlins-MBP.lan>
2024-09-04 00:34:36 -0700

    db stuff

--- go.mod
+++ go.mod
@@ -3,9 +3,29 @@ module github.com/cortlin/mls-ai
 go 1.23
 
 require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/glebarez/sqlite v1.11.0
+	github.com/pressly/goose/v3 v3.21.1
+	gorm.io/gorm v1.25.11
+)
+
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
-	github.com/pressly/goose/v3 v3.21.1 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/sethvargo/go-retry v0.2.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	modernc.org/libc v1.41.0 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.7.2 // indirect
+	modernc.org/sqlite v1.29.6 // indirect
 )
--- main.go
+++ main.go
@@ -5,21 +5,102 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"time"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 )
 
-func PrettyString(str string) (string, error) {
-	var prettyJSON bytes.Buffer
-	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
-		return "", err
+type jsonResponse struct {
+	Hits struct {
+		Hits []jsonResponseInner `json:"hits"`
+	} `json:"hits"`
+}
+
+type jsonResponseInner struct {
+	Source Source `json:"_source"`
+}
+
+type Source struct {
+	Zip           string  `json:"Zip"`
+	StreetName    string  `json:"StreetName"`
+	FullBathrooms string  `json:"FullBathrooms"`
+	Latitude      float64 `json:"Latitude"`
+	Longitude     float64 `json:"Longitude"`
+	StreetSuffix  string
+	StreetAddress string
+	City          string
+	State         string
+	County        string
+	ListingPrice  int
+	HalfBathrooms int
+	Bedrooms      int
+	SquareFeet    int
+}
+
+type Listing struct {
+	gorm.Model
+	zip            string
+	street_name    string
+	street_suffix  string
+	city           string
+	state          string
+	county         string
+	lat            float64
+	lng            float64
+	listing_price  int
+	square_feet    int
+	bedrooms       uint8
+	bathrooms      string
+	half_bathrooms uint8
+}
+
+var db, err = gorm.Open(sqlite.Open("mls-ai.db"), &gorm.Config{})
+
+func StringToListings(str string) jsonResponse {
+	data := jsonResponse{}
+	err := json.Unmarshal([]byte(str), &data)
+
+	if err != nil {
+		fmt.Println(err.Error())
 	}
-	return prettyJSON.String(), nil
+
+	return data
+}
+
+func mlsToDB(mls jsonResponse) []Listing {
+	arr := mls.Hits.Hits
+	r := []Listing{}
+	for _, d := range arr {
+		r = append(r, Listing{
+			zip:            d.Source.Zip,
+			street_name:    d.Source.StreetName,
+			street_suffix:  d.Source.StreetSuffix,
+			city:           d.Source.City,
+			state:          d.Source.State,
+			county:         d.Source.County,
+			lat:            d.Source.Latitude,
+			lng:            d.Source.Longitude,
+			listing_price:  d.Source.ListingPrice,
+			square_feet:    d.Source.SquareFeet,
+			bedrooms:       uint8(d.Source.Bedrooms),
+			bathrooms:      d.Source.FullBathrooms,
+			half_bathrooms: uint8(d.Source.HalfBathrooms),
+		})
+	}
+
+	return r
 }
 
 func main() {
 	url := "https://www.talktotucker.com/api/v1/search/search"
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Listing{})
 
 	fmt.Println("HTTP JSON POST URL:", url)
 
@@ -58,12 +139,19 @@ func main() {
 
 	fmt.Println("response Status:", response.Status)
 	fmt.Println("response Headers:", response.Header)
-	body, _ := io.ReadAll(response.Body)
+	body, err := io.ReadAll(response.Body)
 
-	jsonBody, err := PrettyString(string(body))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
 	}
 
-	fmt.Println("response Body:", jsonBody)
+	jsonBody := StringToListings(string(body))
+
+	localData := mlsToDB(jsonBody)
+	fmt.Println("INSERTING INTO TABLE")
+	if res := db.CreateInBatches(localData, 100); res.Error != nil {
+		panic(res.Error)
+	}
+
+	fmt.Println("TOTAL LISTINGS: ", len(localData))
 }
